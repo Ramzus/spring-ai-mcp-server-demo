d685f4c1c57aa30c44128bc03dae83c00b4635b3
Benjamin Allion <benjamin.allion.pro@gmail.com>
2025-05-16 00:35:12 +0200

    chore(gradle): updates project configuration and dependencies.
    Updates the Java toolchain version to 17.
    Configures Spring Boot dependencies, including web, validation, and data MongoDB starters.
    Adds Spring AI dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,41 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.4.5"
-	id("io.spring.dependency-management") version "1.1.7"
+    java
+    id("org.springframework.boot") version "3.4.5"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.adeo"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(24)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
 }
 
 repositories {
-	mavenCentral()
-	maven {
-		url = uri("https://repo.spring.io/milestone")
-		name = "Spring Milestones"
-	}
+    mavenCentral()
+    maven {
+        url = uri("https://repo.spring.io/milestone")
+        name = "Spring Milestones"
+    }
 }
 
 dependencies {
-	implementation("org.springframework.ai:spring-ai-mcp-server-webmvc-spring-boot-starter:1.0.0-M6")
+    // Spring Core
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 
-	implementation("org.springframework.boot:spring-boot-starter")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // Spring AI
+    implementation("org.springframework.ai:spring-ai-mcp-server-webmvc-spring-boot-starter:1.0.0-M6")
+
+    // Test dependencies
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
