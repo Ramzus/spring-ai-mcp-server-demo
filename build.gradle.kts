51e915786f711ab170f07c9f3b90488b4424e614
Marcel Marquez <marcel.marquez@leroymerlin.fr>
2025-05-28 19:42:12 +0200

    chore: remove gradle binaries in subdirectories in order to keep only the root one

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("java")
-    kotlin("jvm") version "2.1.20" apply false
     id("org.springframework.boot") version "3.4.5" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
 }
