cdb0225c31daefdd07393d73d97c5cc450fedc80
Marcel Marquez <marcel.marquez@outlook.com>
2025-06-01 10:06:08 +0200

    chore: project init

---
+++ oms-mcp-server/src/main/java/com/example/oms/mcp/server/OmsMcpServer.java
@@ -0,0 +1,13 @@
+package com.example.oms.mcp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmsMcpServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmsMcpServer.class, args);
+	}
+
+}
---
+++ oms-mcp-server/src/test/java/com/example/oms/mcp/server/OmsMcpServerTests.java
@@ -0,0 +1,13 @@
+package com.example.oms.mcp.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsMcpServerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
